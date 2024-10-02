728ea762704ca49f02a3bc4a9e9effcea6023c98
IBM Academy: Hybrid Cloud and Red Hat @ Infinite Learning Indonesia <infinitelearningibmhcrh@gmail.com>
2024-10-02 13:40:38 +0700

    Update and rename INSTAL.sh to installl.sh

--- installl.sh
+++ installl.sh
@@ -8,7 +8,6 @@ sudo chmod a+x /usr/bin/lab &>/dev/null
 sudo rm -rf /tmp/sh &>/dev/null
 useradd student &>/dev/null
 echo "student" | passwd --stdin student &>/dev/null
-echo -e "\e[1;42;97m LAB INSTALLED, YOU CAN LAB START NOW !! \e[0m"
-echo ""
 su - student 
+
 
