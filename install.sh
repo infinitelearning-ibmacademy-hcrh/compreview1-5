db4133748d6058c8ba9aac3a6d29dfd53ee3892d
IBM Academy: Hybrid Cloud and Red Hat @ Infinite Learning Indonesia <infinitelearningibmhcrh@gmail.com>
2024-10-02 13:31:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@
 sudo dnf install git -y &>/dev/null
 sudo mkdir -p /tmp/sh &>/dev/null
 sudo chmod 777 /tmp/sh  &>/dev/null
-git clone https://github.com/omidiyanto/lab.git /tmp/sh &>/dev/null
+git clone https://github.com/infinitelearning-ibmacademy-hcrh/compreview1-5.git /tmp/sh &>/dev/null
 sudo cp /tmp/sh/lab.sh /usr/bin/lab &>/dev/null
 sudo chmod a+x /usr/bin/lab &>/dev/null
 sudo rm -rf /tmp/sh &>/dev/null
 echo -e "\e[1;42;97m LAB INSTALLED, YOU CAN LAB START NOW !! \e[0m"
 echo ""
-
-
-
-curl -s https://raw.githubusercontent.com/omidiyanto/lab/install/Install.sh | bash
